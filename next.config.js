8e36b532d9af4ee9826cdf4551bdd4ac995f341d
Shivansh Mehta <64299373+shivdon@users.noreply.github.com>
2021-06-16 11:47:37 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => {
-  return {
-    env: {
-      NEXT_PUBLIC_SEND_MESSAGE: "/api/contact",
-      DATABASE:
-        "mongodb+srv://admin-shivansh:ETCMQjh7cB2xWJgk@cluster0.lulk0.mongodb.net/blog-next?retryWrites=true&w=majority",
-    },
-  };
-};
